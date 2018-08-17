fa9a391fd45f9506b2132118e335d9f660a82393
Hai Tran <haitranviet96@gmail.com>
2018-08-17 17:05:46 +0700

    add bootstrap

--- gamification/src/test/java/microservices/book/gamification/service/LeaderBoardServiceImplTest.java
+++ gamification/src/test/java/microservices/book/gamification/service/LeaderBoardServiceImplTest.java
@@ -13,9 +13,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
-/**
- * @author moises.macero
- */
 public class LeaderBoardServiceImplTest {
 
     private LeaderBoardServiceImpl leaderBoardService;
